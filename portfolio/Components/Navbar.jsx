f541fa27871f1c0364d599cd78219cd7ddde5122
Haarika Ramadugu <hramadugu@gmail.com>
2023-03-09 15:59:05 -0800

    feat:added navbar

---
+++ portfolio/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+const Navbar = () => {
+
+    return (
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+            <li>
+                <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+                <Link href="/projects">Projects</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Navbar
--- portfolio/src/app/home/page.jsx
+++ portfolio/src/app/home/page.jsx
@@ -1,8 +1,7 @@
-import Navbar from "Components/Navbar";
+
 export default function Page() {
     return (
         <div>
-            <Navbar />
             <h1>Haarika Ramadugu</h1>
             <p>A FullStack focussed web developer building the full stack applications that leads to the sucess of the overall product</p>
         </div>);
