ae6959668cb566c37b61425f2d31c97a2368958b
Haarika Ramadugu <hramadugu@gmail.com>
2023-09-22 14:58:20 -0700

    feat:polished navbar for new navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import "bootstrap/dist/css/bootstrap.min.css";
 // // import React, { useEffect } from 'react'
 
+
 export default function Navbar() {
   //   // const [scrolled, setScrolled] = React.useState(false);
 
@@ -20,77 +21,46 @@ export default function Navbar() {
   //   //   window.addEventListener('scroll', handleScroll)
   //   // })
 
+
   return (
-    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" href="/">
+          <Image src="/static/logo.png" width={70} height={70} />
+        </Link>
 
-      {/* // <nav class="site-nav js-site-navbar mb-5 site-navbar-target scrolled awake">
-// <div class="container">
-// <div class="site-navigation text-center">
-// <a href="index.html" class="d-block text-center float-left logo">JA</a>
-// <ul class="js-clone-nav ml-0 pl-0 d-none d-lg-inline-block site-menu mx-auto text-center">
-// <li class="active"><a href="#home-section" class="nav-link">Home</a></li>
-// <li class="has-children">
-// <a href="#" class="nav-link">Dropdown</a>
-// <ul class="dropdown">
-// <li><a href="#testimonials-section" class="nav-link">Testimonials</a></li>
-// <li class="has-children">
-// <a href="#">Menu Two</a>
-// <ul class="dropdown">
-// <li><a href="#" class="nav-link">Sub Menu One</a></li>
-// <li><a href="#" class="nav-link">Sub Menu Two</a></li>
-// <li><a href="#" class="nav-link">Sub Menu Three</a></li>
-// </ul>
-// </li>
-// <li><a href="#" class="nav-link">Menu Three</a></li>
-// </ul>
-// </li>
-// <li><a href="#about-section" class="nav-link">About</a></li>
-// <li><a href="#portfolio-section" class="nav-link">Portfolio</a></li> */}
-      {/* // <li><a href="#services-section" class="nav-link">Services</a></li>
-// <li><a href="#resume-section" class="nav-link">Resume</a></li>
-// <li><a href="#blog-section" class="nav-link active">Blog</a></li>
-// <li><a href="#contact-section" class="nav-link">Contact</a></li>
-// </ul> */}
-      {/* // <a href="#" class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block light d-lg-none" data-toggle="collapse" data-target="#main-navbar">
-// <span></span>
-// </a>
-// </div>
-// </div>
-// </nav> */}
-      <nav className='navbar navbar-md navbar-light' >
-        <div className="navbar-brand" href="#"><Image src="/static/logo.png" width={70} height={70} /></div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#main-navbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        <ul className='nav justify-content-center my-4' role="navigation">
-          <li className='nav-item '>
-            <Link className='nav-link active text-md' href="/" style={{
-              color: "#000000"
-            }}>Home</Link>
-          </li>
-          <li className='nav-item '>
-            <Link className='nav-link active' href="/about" style={{
-              color: "#000000"
-            }}>About</Link>
-          </li>
-
-          <li className='nav-item '>
-            <Link className='nav-link' href="/projects" style={{
-              color: "#000000"
-            }}>Projects</Link>
-
-          </li>
-        </ul >
-
-      </nav >
-    </>
-  )
-}
-/* 
+        <div className="collapse navbar-collapse" id="main-navbar">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" href="#home-section">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/projects">
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 
-//  href="/resume"
- 
-//  <li className='nav-item py-3 py-lg-4'>
-//           <Link className='nav-link active' style={{
-//             color: "#000000"
-//           }}>Contact</Link>
-//         </li>*/
+
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,19 +6,19 @@ export default function Page() {
     return (
 
         <div className="d-flex justify-content-center py-5 px-5" >
-            <div className="mt-5 pt-5 py-5 col-lg-5">
+            <div className="mt-5 pt-5 py-5 col-lg-6">
                 <span className="display mt-5 pt-5"><h3>Full Stack Developer</h3></span>
                 <h1 className="mb-4">Haarika Ramadugu</h1>
                 <div className="text-wrap">
-                    <p className="w-20 text-lg"> specialising in
+                    <p className="mw-60  text-lg"> specialising in
                         building customer-centric web applications
                         that solve real world problems
                         and provide seamless user experience.</p>
                     {/* <HireMebutton /> */}
                 </div>
             </div>
-            <div class="card" style={{ width: 70 + 'rem', height: 200 }}>
-                <img src="/static/portfolio2.jpeg" class="card-img-top" alt="profile"></img>
+            <div class="card" style={{ width: 40 + 'rem' }}>
+                <img src="/static/portfolio2.jpeg" className="card-img-top" alt="profile"></img>
             </div>
             {/* <Link href="https://www.linkedin.com/in/haarika-ramadugu/" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="#0a66c2" marginLeft="10" class="bi bi-linkedin" viewBox="0 0 16 16">
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
