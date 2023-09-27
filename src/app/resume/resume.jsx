9be66a0f76d87edb9a8739113aa61f6865b678b1
Haarika Ramadugu <hramadugu@gmail.com>
2023-09-27 14:27:40 -0700

    fixed:project structore of pages

--- src/app/resume/resume.jsx
+++ src/app/resume/resume.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Experience from "../../Components/Experience";
+import Experience from "../Components/Experience";
 import React from "react";
 export default function Resume() {
     return (
